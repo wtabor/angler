66c6293efacea621e7fac0c9bdf5efca12f9c0dc
Will Tabor <tabor.will@gmail.com>
2016-04-25 10:53:32 -0400

    Updated files

--- config/application.rb
+++ config/application.rb
@@ -8,6 +8,10 @@ Bundler.require(*Rails.groups)
 
 module Angler
   class Application < Rails::Application
+    config.generators do |g|
+      g.orm             :active_record
+      g.template_engine :erb
+    end
     # Settings in config/environments/* take precedence over those specified here.
     # Application configuration should go into files in config/initializers
     # -- all .rb files in that directory are automatically loaded.
