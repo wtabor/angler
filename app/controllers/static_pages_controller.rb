4e7f9ff2d84860b80aa1708de17dedf56a84480e
Will Tabor <tabor.will@gmail.com>
2016-04-25 14:58:22 -0400

    Finished static pages

--- app/controllers/static_pages_controller.rb
+++ app/controllers/static_pages_controller.rb
@@ -1,19 +1,10 @@
 class StaticPagesController < ApplicationController
-<<<<<<< HEAD
   def home
-  end
-=======
-
-  def home
-  end
-
-  def help
   end
 
   def about
   end
 
-  def contact
-  end
->>>>>>> static-pages
+
+
 end
