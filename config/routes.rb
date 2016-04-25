5fe09012436fa026f7ed34a2c26ed9d5a91b81b3
Will Tabor <tabor.will@gmail.com>
2016-04-25 13:44:37 -0400

    Added static pages

--- config/routes.rb
+++ config/routes.rb
@@ -1,14 +1,5 @@
 Rails.application.routes.draw do
   root                'static_pages#home'
   get    'help'    => 'static_pages#help'
-  resources :users do
-    member do
-      get :following, :followers
-    end
-  end
-  resources :users
-  resources :account_activations, only: [:edit]
-  resources :password_resets,     only: [:new, :create, :edit, :update]
-  resources :microposts,          only: [:create, :destroy]
-  resources :relationships,       only: [:create, :destroy]
+  get    'about'   => 'static_pages#about'
 end
